5f4db4e1a5a240f9585b7c240b61d6b697d24580
Shirley Wu <shirleyxueyangwu@gmail.com>
2020-03-18 23:09:49 -0700

    setup store

---
+++ src/store/index.js
@@ -0,0 +1,24 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  state: {
+    day: 0,
+    people: [],
+    numHouses: 0,
+    destinations: [],
+  },
+  getters: {
+
+  },
+  mutations: {
+
+  },
+  actions: {
+    infect ({ commit }) {
+
+    }
+  }
+})
