d83dae017eef4d4dc1a4c06c9e4dd32ed064adef
Shirley Wu <shirleyxueyangwu@gmail.com>
2020-04-18 16:09:04 -0700

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   lintOnSave: false,
-  publicPath: process.env.NODE_ENV === 'production' ? '/covid19/' : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? '/' : '/',
 
   css: {
     loaderOptions: {
